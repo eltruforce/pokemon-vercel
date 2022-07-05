6fe4ebde3f20dac914d1bcdba82d170ecb3fc987
eltruforce <lordace550@gmail.com>
2022-07-04 20:07:55 -0500

    arrow function alternative to the components

--- components/Background/index.tsx
+++ components/Background/index.tsx
@@ -3,10 +3,7 @@ import { Scene, WebGL1Renderer, PerspectiveCamera } from "three";
 import Renderer from "./renderer";
 
 export default class Background extends Component {
-  componentDidMount(): void {
-    new Renderer();
-  }
-  render() {
-    return <canvas id="bg" />;
-  }
+  componentDidMount = () => new Renderer();
+
+  render = () => <canvas id="bg" />;
 }
--- components/Background/renderer.tsx
+++ components/Background/renderer.tsx
@@ -4,7 +4,14 @@ import Scene from "./scenes/scene";
 export default class Renderer extends WebGL1Renderer {
   constructor() {
     super({ antialias: true, canvas: document.getElementById("bg") });
-    this.setSize(window.innerWidth, window.innerHeight);
+    this.config();
     new Scene(this);
   }
+
+  private config() {
+    this.setSize(window.innerWidth, window.innerHeight);
+    window.addEventListener("resize", this.resize);
+  }
+
+  private resize = () => window.location.reload();
 }
