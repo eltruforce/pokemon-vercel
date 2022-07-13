6d1ff47e57a54dacb039a12e1e2ff332d496b422
eltruforce <lordace550@gmail.com>
2022-07-12 19:57:52 -0500

    add lights to the models

--- components/Background/class/lights.tsx
+++ components/Background/class/lights.tsx
@@ -1,8 +1,11 @@
-import { AmbientLight, HemisphereLight } from "three";
+import { AmbientLight, DirectionalLight, HemisphereLight } from "three";
 
 export default class Lights {
   constructor(scene) {
     scene.add(new AmbientLight(0xffffff, 0.8));
     scene.add(new HemisphereLight(0xffffff, 0.8));
+    const sun = new DirectionalLight(0xfffff, 0.5);
+    sun.position.set(20, 20, 5);
+    scene.add(sun);
   }
 }
